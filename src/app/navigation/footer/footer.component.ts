677a8d6abdc9538fc4d1fb73cdea0b965bd21100
Davide Bonifacio <web@davidebonifacio.it>
2019-02-22 19:41:25 +0100

    header and footer hide on scroll down

--- src/app/navigation/footer/footer.component.ts
+++ src/app/navigation/footer/footer.component.ts
@@ -1,19 +1,89 @@
 import {
   Component,
   OnInit,
+  AfterViewInit,
+  HostBinding,
   OnDestroy
 } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
 
 import { AuthService } from '../../auth/auth.service';
 
-import { Subscription } from 'rxjs';
+import { Subscription, fromEvent } from 'rxjs';
+
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pairwise,
+  share,
+  tap,
+  throttleTime
+} from 'rxjs/operators';
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ opacity: 0, transform: 'translateY(100%)' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ]
 })
-export class FooterComponent implements OnInit, OnDestroy {
+export class FooterComponent implements OnInit, AfterViewInit, OnDestroy {
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
+
+  ngAfterViewInit() {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$.subscribe(() => (this.isVisible = true));
+    goingDown$.subscribe(() => (this.isVisible = false));
+  }
+
   userIsAuthenticated = false;
   private authListenerSub: Subscription;
 
