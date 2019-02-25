23c33681646f767802191be07241934b982b14bb
Davide Bonifacio - Studiokey <web@davidebonifacio.it>
2019-02-25 18:05:21 +0100

    testing Facebook authentication

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -17,6 +17,24 @@ export class LoginComponent {
   constructor(public authService: AuthService) {}
 
   ngOnInit() {
+    (window as any).fbAsyncInit = function() {
+      FB.init({
+        appId      : '375502226629393',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.1'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
+
     this.authStatusSub = this.authService.getAuthStatusListener().subscribe(
       authStatus => {
         this.isLoading = false;
@@ -29,6 +47,19 @@ export class LoginComponent {
       return;
     }
     this.authService.login(form.value.email, form.value.password);
+  }
+
+  fbLogin(){
+    FB.login((response)=> {
+      console.log('submitLogin',response);
+      if (response.authResponse) {
+        FB.api('/me', function(response) {
+          console.log(response);
+        });
+      } else {
+        console.log('User login failed');
+      }
+    });
   }
 
   ngOnDestroy() {
